3251e9ea34952418879c614c19eb5b40aabd225e
MrClaro <adryan.contatoprofissional@gmail.com>
2026-01-08 01:53:29 -0300

    feat: create home page with templates

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import BaseLayout from "../components/layout/BaseLayout.vue";
+import Header from "../components/layout/Header.vue";
+import Footer from "../components/layout/Footer.vue";
+import PhotoGrid from "../components/features/PhotoGrid.vue";
+</script>
+
+<template>
+  <BaseLayout>
+    <template #header><Header /></template>
+    <template #default>
+      <PhotoGrid />
+    </template>
+    <template #footer><Footer /></template>
+  </BaseLayout>
+</template>
+
+<style scoped></style>
