0645a0bae92ff402e34156d1c306946a03539bbd
MrClaro <adryan.contatoprofissional@gmail.com>
2026-01-08 01:53:53 -0300

    feat: implement header component with centered title

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Galeria de Fotos</h1>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  margin: 0;
+  padding: 3.125rem 4.375rem 0.875rem 4.375rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  position: relative;
+  animation: fadeInDown 0.6s ease-out;
+
+  h1 {
+    font-size: 2.5rem;
+    color: #333;
+    margin: 0;
+  }
+}
+</style>
