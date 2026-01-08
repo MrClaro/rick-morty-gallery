c63befc6148eff004231c72a3cce9633fb3046f9
MrClaro <adryan.contatoprofissional@gmail.com>
2026-01-08 01:54:14 -0300

    feat: create BaseLayout to orchestrate app structure

---
+++ src/components/layout/BaseLayout.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main><slot></slot></main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
+
+<style lang="scss"></style>
